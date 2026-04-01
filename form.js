40f19caec3c842a68691f7da9f92cdbf5866f692
invitacionesweb <invitacionesweb.info@gmail.com>
2026-04-01 14:46:19 -0300

    a
    a

---
+++ form.js
@@ -0,0 +1,34 @@
+const scriptUrl = 'https://script.google.com/macros/s/AKfycby690PMpCRkYKy-09UAG0m7UKkTDelZxJMc9c9PE5oJnLXVJTb5PwNc2v5pVH5W_666/exec';
+const form = document.forms['asistenciaform'];
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  Swal.fire({
+    title: 'Enviando...',
+    text: 'Por favor, esperá un momento',
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    didOpen: () => {
+      Swal.showLoading();
+    }
+  });
+
+  fetch(scriptUrl, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+      Swal.fire({
+        title: "¡MUCHAS GRACIAS!",
+        text: "Formulario Enviado",
+        icon: "success"
+      });
+    })
+    .then(() => {
+      setTimeout(() => {
+        window.location.reload();
+      }, 1500); // da tiempo a leer el mensaje
+    })
+    .catch(error => {
+      Swal.fire("Error", "No se pudo enviar el formulario. Intentá de nuevo.", "error");
+      console.error('Error', error.message);
+    });
+});
